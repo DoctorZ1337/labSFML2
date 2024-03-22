6267286d8173cf7c70ea0fb8304b7a2f4a525159
DoZ1337 <zenchenkoslava@mail.ru>
2024-03-22 19:42:26 +0200

    cmon baby

---
+++ inc/Text.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <string>
+
+namespace sz
+{
+    class MyText
+    {
+        std::string m_text;
+        float m_ShowTime;
+        sf::RenderWindow& m_window;
+        sf::Font m_font;
+        sf::SoundBuffer m_buffer;
+        sf::Sound m_sound;
+        sf::Color getRandomColor() const
+        {
+            return sf::Color(rand() % 256, rand() % 256, rand() % 256);
+        }
+
+    public:
+        MyText(sf::RenderWindow& window, const std::string& text, float ShowTime)
+            : m_window(window), m_text(text), m_ShowTime(ShowTime)
+        {
+            if (!m_font.loadFromFile("assets\\arial.ttf"))
+            {
+                std::cout << "Error while loading arial.ttf" << std::endl;
+            }
+
+            if (!m_buffer.loadFromFile("assets\\sound.wav"))
+            {
+                std::cout << "Error while loading sound.wav" << std::endl;
+            }
+            m_sound.setBuffer(m_buffer);
+        }
+
+        void verbAnimationText()
+        {
+            sf::Text text;
+            text.setFont(m_font);
+            text.setCharacterSize(30);
+            text.setStyle(sf::Text::Bold);
+
+            sf::Clock clock;
+
+            float charShowTime = m_ShowTime / static_cast<float>(m_text.size());
+
+            sf::Color currentColor = getRandomColor();
+            m_window.clear(currentColor);
+
+            for (int i = 0; i < m_text.size(); ++i)
+            {
+                if (m_text[i] != ' ' && !ispunct(m_text[i]))
+                {
+                    currentColor = getRandomColor();
+                }
+
+                m_window.clear(currentColor);
+
+                text.setString(m_text.substr(0, i + 1));
+
+                m_window.draw(text);
+                m_window.display();
+
+                sf::sleep(sf::seconds(charShowTime));
+            }
+
+            m_sound.play();
+
+            sf::sleep(sf::seconds(2));
+        }
+    };
+}
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <SFML/Graphics.hpp>
-#include <Circle.hpp>
+#include <Text.hpp>
 #include <Game.hpp>
 
 int main()
 {
-    mt::Game game(1000, 600, "Game");
+    sz::Game game(1000, 600, "Game");
     
     if (!game.Setup(10))
         return -1;
